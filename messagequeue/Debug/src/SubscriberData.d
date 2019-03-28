ee9cb0042acfe8c205c643bf3bfc39b1bff9305e
mjamilfarooq <jamil.farooq@hotmail.com>
2019-03-29 08:06:27 +1100

    deleting Debug folder from repo

--- messagequeue/Debug/src/SubscriberData.d
+++
@@ -1,6 +0,0 @@
-src/SubscriberData.d: ../src/SubscriberData.cpp ../src/SubscriberData.h \
- ../src/Message.h
-
-../src/SubscriberData.h:
-
-../src/Message.h:
--- messagequeue/Debug/src/messagequeue.d
+++
@@ -1,6 +0,0 @@
-src/messagequeue.d: ../src/messagequeue.cpp ../src/AllTests.h \
- ../src/IMessageQueueManager.h
-
-../src/AllTests.h:
-
-../src/IMessageQueueManager.h:
